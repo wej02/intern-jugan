4b67be6bcfc8df06631b057e71f913d46be63a53
johnmikosugbodoc <johnmiko.sugbodoc@gmail.com>
2023-03-01 14:30:13 +0800

    Added New Cards for Today Revenue, Unique Visitors and Expensesin Dashboard 01 View

--- public/assets/js/index1.js
+++ public/assets/js/index1.js
@@ -110,6 +110,104 @@ $(function(e){
 		}
 	});
 	/*leads end */
+
+	/* new leads */
+    var ctx = $('#new-leads');
+	ctx.height(230);
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: ["May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			type: 'line',
+			datasets: [{
+				label: "Sales",
+				data: [320, 440, 510, 604, 126, 758, 264, 945],
+				backgroundColor: 'rgba(68, 84, 195,0.1)',
+				borderColor: 'rgba(68, 84, 195,0.9)',
+				borderWidth: 5,
+				pointStyle: 'circle',
+				pointRadius: 0,
+				pointBorderColor: 'transparent',
+				pointBackgroundColor: 'rgba(68, 84, 195,0.8)',
+			}]
+		},
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			tooltips: {
+				mode: 'index',
+				titleFontSize: 12,
+				titleFontColor: 'rgba(0,0,0,0.9)',
+				bodyFontColor: 'rgba(0,0,0,0.9)',
+				backgroundColor: '#fff',
+				bodyFontFamily: 'Montserrat',
+				cornerRadius: 0,
+				intersect: false,
+			},
+			legend: {
+				display: false,
+				labels: {
+					usePointStyle: true,
+					fontFamily: 'Montserrat',
+				},
+			},
+			scales: {
+				xAxes: [{
+					display: true,
+					ticks: {
+						display: true,
+						fontColor: "#8e9cad",
+						fontSize: "13",
+					},
+					scaleLabel: {
+						display: true,
+						labelString: 'Months',
+						fontSize: "15",
+						fontColor: "#8e9cad",
+					},
+					gridLines: {
+						display: true,
+						drawBorder: false,
+						color: 'rgba(193, 184, 184,0.1)',
+						zeroLineColor: '#000'
+					}
+				}],
+				yAxes: [{
+					display: true,
+					ticks: {
+						display: true,
+						fontColor: "#8e9cad",
+						color: 'rgba(193, 184, 184,0.1)',
+						fontSize: "13",
+						callback: function(value, index, values) {
+							return '$' + value;
+						},
+						maxRotation: 0,
+						stepSize: 50,
+						min: 0,
+						max: 1000
+					},
+					scaleLabel: {
+						display: true,
+						labelString: 'Revenue',
+						fontColor: "#8e9cad",
+						fontSize: "15",
+					},
+					gridLines: {
+						display: true,
+						drawBorder: false,
+						color: 'rgba(193, 184, 184,0)',
+						zeroLineColor: 'rgba(193, 184, 184,0.1)'
+					}
+				}]
+			},
+			title: {
+				display: false,
+				text: 'Normal Legend'
+			}
+		}
+	});
+	/*new leads end */
 	
 	
 	/*sparkline*/
@@ -170,7 +268,7 @@ $(function(e){
 	var spark1 = new ApexCharts(document.querySelector("#spark1"), spark1);
     spark1.render();
   
-	var sparklineData2 = [0, 45, 93, 53, 61, 27, 54, 43, 19, 46, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, ];
+	var sparklineData2 = [0, 45, 93, 53, 61, 27, 54, 43, 19, 46, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27];
 	//Spark2
     var spark2 = {
       chart: {
@@ -253,6 +351,137 @@ $(function(e){
     }
 	var spark3 = new ApexCharts(document.querySelector("#spark3"), spark3);
     spark3.render();
+
+	var sparklineData4 = [10, 33, 21, 95, 23, 93, 73, 61, 42, 54, 43, 19, 46,45, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51];
+	//Spark4
+    var spark4 = {
+		chart: {
+		  type: 'area',
+		  height: 60,
+		  width: 160,
+		  sparkline: {
+			enabled: true
+		  },
+		  dropShadow: {
+			  enabled: true,
+			  blur: 3,
+			  opacity: 0.2,
+		  }
+		  },
+		  stroke: {
+			  show: true,
+			  curve: 'smooth',
+			  lineCap: 'butt',
+			  colors: undefined,
+			  width: 2,
+			  dashArray: 0,      
+		  },
+		  fill: {
+		  gradient: {
+			enabled: false
+		  }
+		},
+		series: [{
+		  name: 'New Today Revenue',
+		  data: randomizeArray(sparklineData4)
+		}],
+		yaxis: {
+		  min: 0
+		},
+		colors: ['#4454c3'],
+  
+	  }
+	  var spark4 = new ApexCharts(document.querySelector("#spark4"), spark4);
+	  spark4.render();
+
+
+	  var sparklineData5 = [0, 25, 31, 37, 25, 33, 53, 60, 90, 12, 55, 67, 27, 72, 12, 69, 99, 72, 54, 27, 93, 21, 22, 91];
+	  //Spark5
+	  var spark5 = {
+		chart: {
+		  type: 'area',
+		  height: 60,
+		  width: 160,
+		  sparkline: {
+			enabled: true
+		  },
+		  dropShadow: {
+			  enabled: true,
+			  blur: 3,
+			  opacity: 0.2,
+		  }
+		  },
+		  stroke: {
+			  show: true,
+			  curve: 'smooth',
+			  lineCap: 'butt',
+			  colors: undefined,
+			  width: 2,
+			  dashArray: 0,      
+		  },
+		  fill: {
+		  gradient: {
+			enabled: false
+		  }
+		},
+		series: [{
+		  name: 'New Unique Visitors',
+		  data: randomizeArray(sparklineData5)
+		}],
+		yaxis: {
+		  min: 0
+		},
+		colors: ['#2dce89'],
+  
+	  }
+	  var spark5 = new ApexCharts(document.querySelector("#spark5"), spark5);
+	  spark5.render();
+
+	  var sparklineData6 = [0, 23, 45, 78, 55, 13, 63, 31, 97, 14, 53, 29, 66, 75, 52, 18, 66, 84, 55, 11, 87, 59, 22, 91];
+
+	  for(var i = 0; i<1000; i++){
+
+	  }
+	  //Spark6
+	  var spark6 = {
+		chart: {
+		  type: 'area',
+		  height: 60,
+		  width: 160,
+		  sparkline: {
+			enabled: true
+		  },
+		  dropShadow: {
+			  enabled: true,
+			  blur: 3,
+			  opacity: 0.2,
+		  }
+		  },
+		  stroke: {
+			  show: true,
+			  curve: 'smooth',
+			  lineCap: 'butt',
+			  colors: undefined,
+			  width: 2,
+			  dashArray: 0,      
+		  },
+		  fill: {
+		  gradient: {
+			enabled: false
+		  }
+		},
+		series: [{
+		  name: 'New Expenses',
+		  data: randomizeArray(sparklineData6)
+		}],
+		yaxis: {
+		  min: 0
+		},
+		colors: ['#ff5b51'],
+  
+	  }
+	  var spark6 = new ApexCharts(document.querySelector("#spark6"), spark6);
+	  spark6.render();
 	
 	/*----P-scrolling JS ----*/
 	const ps31 = new PerfectScrollbar('.countryscroll', {
