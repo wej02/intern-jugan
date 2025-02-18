3d703c017b69f3cc96796761eed62a49a62508be
louie <louie.sugbodoc@gmail.com>
2025-02-18 16:51:36 +0800

    modified app-calendar-events.js Added my birthday/schedule of internship/study load schedule modified calendar.php added breakline <br> between start date and description

--- app/Views/pages/calendar.php
+++ app/Views/pages/calendar.php
@@ -167,7 +167,7 @@
 								<label class="tx-13 mg-b-2">End Date</label>
 								<p class="event-end-date"></p>
 							</div>
-						</div><label class="tx-13 tx-gray-600 mg-b-2">Description</label>
+						</div><label class="tx-13 tx-gray-600 mg-b-2"><br>Description:</label>
 						<p class="event-desc tx-gray-900 mg-b-30"></p><a class="btn btn-secondary wd-80" data-dismiss="modal" href="">Close</a>
 					</div>
 				</div>
--- public/assets/js/app-calendar-events.js
+++ public/assets/js/app-calendar-events.js
@@ -53,6 +53,38 @@ var sptCalendarEvents = {
 		backgroundColor: 'rgba(68, 84, 195, 0.15)',
 		borderColor: 'rgba(68, 84, 195, 0.15)',
 		description: 'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary'
+	}, {
+		id: '20',
+		start: curYear + '-' + curMonth + '-01T19:00:00',
+		end: curYear + '-' + curMonth + '-01T22:00:00',
+		title: 'Capstone Schedule',
+		backgroundColor: 'rgba(68, 84, 195, 0.15)',
+		borderColor: 'rgba(68, 84, 195, 0.15)',
+		description: 'Study load for the first semester'
+	}, {
+		id: '21',
+		start: curYear + '-' + curMonth + '-08T19:00:00',
+		end: curYear + '-' + curMonth + '-08T22:00:00',
+		title: 'Capstone Schedule',
+		backgroundColor: 'rgba(68, 84, 195, 0.15)',
+		borderColor: 'rgba(68, 84, 195, 0.15)',
+		description: 'Study load for the first semester'
+	}, {
+		id: '22',
+		start: curYear + '-' + curMonth + '-15T19:00:00',
+		end: curYear + '-' + curMonth + '-15T22:00:00',
+		title: 'Capstone Schedule',
+		backgroundColor: 'rgba(68, 84, 195, 0.15)',
+		borderColor: 'rgba(68, 84, 195, 0.15)',
+		description: 'Study load for the first semester'
+	}, {
+		id: '23',
+		start: curYear + '-' + curMonth + '-22T19:00:00',
+		end: curYear + '-' + curMonth + '-22T22:00:00',
+		title: 'Capstone Schedule',
+		backgroundColor: 'rgba(68, 84, 195, 0.15)',
+		borderColor: 'rgba(68, 84, 195, 0.15)',
+		description: 'Study load for the first semester'
 	}]
 };
 // Birthday Events Source
@@ -61,6 +93,14 @@ var sptBirthdayEvents = {
 	backgroundColor: 'rgba(247, 45, 102, 0.15)',
 	borderColor: 'rgba(247, 45, 102, 0.15)',
 	events: [{
+		id: '19',
+		start: curYear + '-11-12T00:00:00',
+		end: curYear + '-11-12T23:59:00',
+		title: 'Louie Jay’s Birthday',
+		backgroundColor: 'rgba(247, 45, 102, 0.15)',
+		borderColor: 'rgba(247, 45, 102, 0.15)',
+		description: 'Celebrating Louie Jay’s Birthday'	
+	},	{
 		id: '7',
 		start: curYear + '-' + curMonth + '-04T18:00:00',
 		end: curYear + '-' + curMonth + '-04T23:30:00',
@@ -117,5 +157,125 @@ var sptOtherEvents = {
 		start: curYear + '-' + curMonth + '-29',
 		end: curYear + '-' + curMonth + '-31',
 		title: 'My Rest Day'
+	}, {
+		id: '24',
+		start: curYear + '-' + curMonth + '-03T09:00:00',
+		end: curYear + '-' + curMonth + '-03T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '25',
+		start: curYear + '-' + curMonth + '-04T09:00:00',
+		end: curYear + '-' + curMonth + '-04T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '26',
+		start: curYear + '-' + curMonth + '-05T09:00:00',
+		end: curYear + '-' + curMonth + '-05T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '27',
+		start: curYear + '-' + curMonth + '-06T09:00:00',
+		end: curYear + '-' + curMonth + '-06T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '28',
+		start: curYear + '-' + curMonth + '-07T09:00:00',
+		end: curYear + '-' + curMonth + '-07T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '30',
+		start: curYear + '-' + curMonth + '-10T09:00:00',
+		end: curYear + '-' + curMonth + '-10T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '31',
+		start: curYear + '-' + curMonth + '-11T09:00:00',
+		end: curYear + '-' + curMonth + '-11T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '32',
+		start: curYear + '-' + curMonth + '-12T09:00:00',
+		end: curYear + '-' + curMonth + '-12T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '33',
+		start: curYear + '-' + curMonth + '-13T09:00:00',
+		end: curYear + '-' + curMonth + '-13T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '34',
+		start: curYear + '-' + curMonth + '-14T09:00:00',
+		end: curYear + '-' + curMonth + '-14T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '35',
+		start: curYear + '-' + curMonth + '-17T09:00:00',
+		end: curYear + '-' + curMonth + '-17T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '36',
+		start: curYear + '-' + curMonth + '-18T09:00:00',
+		end: curYear + '-' + curMonth + '-18T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '37',
+		start: curYear + '-' + curMonth + '-19T09:00:00',
+		end: curYear + '-' + curMonth + '-19T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '38',
+		start: curYear + '-' + curMonth + '-20T09:00:00',
+		end: curYear + '-' + curMonth + '-20T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '39',
+		start: curYear + '-' + curMonth + '-21T09:00:00',
+		end: curYear + '-' + curMonth + '-21T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '40',
+		start: curYear + '-' + curMonth + '-24T09:00:00',
+		end: curYear + '-' + curMonth + '-24T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '41',
+		start: curYear + '-' + curMonth + '-25T09:00:00',
+		end: curYear + '-' + curMonth + '-25T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '42',
+		start: curYear + '-' + curMonth + '-26T09:00:00',
+		end: curYear + '-' + curMonth + '-26T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '43',
+		start: curYear + '-' + curMonth + '-27T09:00:00',
+		end: curYear + '-' + curMonth + '-27T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
+	}, {
+		id: '44',
+		start: curYear + '-' + curMonth + '-28T09:00:00',
+		end: curYear + '-' + curMonth + '-28T18:00:00',
+		title: 'Internship Schedule',
+		description: 'Internship Schedule'
 	}]
 };
